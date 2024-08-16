f3f3142a4143ed09d8a8df2a958dde0bad734d37
Konkumuttisandeepkumar <94606423+Konkumuttisandeepkumar@users.noreply.github.com>
2024-08-16 16:09:27 +0530

    Add files via upload

---
+++ 1. Input and Output/main.cpp
@@ -0,0 +1,12 @@
+/***WAP to add 2 numbers***/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=0,b=0,sum=0;
+    cout<<"Enter a values of a & b:"<<endl;
+    cin>>a>>b;
+    sum = a+b;
+    cout<<" The Sum of " << a << " and " << b << " is " << sum <<endl;
+    return 0;
+}
